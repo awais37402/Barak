36e96e93d57309fdd348ab6d80d7de107c8bd009
awais37402 <awaistahir01234@gmail.com>
2025-04-11 18:20:54 +0500

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section className="about-section">
+      <div className="container">
+        <h2 className="about-title">Barak AgriTech</h2>
+        <div className="about-content">
+          <p>
+          Pioneering a New Era in Pakistani Agriculture
+          Barak AgriTech (Private) Limited is an agricultural technology company dedicated to transforming farming in Pakistan. From our headquarters in Islamabad, we bring innovative solutions to farmers, empowering them to grow more with less. With a proven track record of supporting over 100s farmers and enhancing productivity across 50,00+ acres, we’re committed to making agriculture sustainable, profitable, and resilient.          </p>
+          <p>
+          Founded and led by industry experts, Barak AgriTech is driven by a vision to modernize farming through cutting-edge technology. With a passion for innovation and a deep understanding of Pakistan’s agricultural challenges, our experts are steering the company to deliver solutions that boost yields, save resources, and secure the future of farming.           </p>
+          <p>
+          We’ve partnered with Magnetic Technologies LLC, a Dubai-based global leader in magnetic energy applications since 1995. With over 30 years of expertise across 50+ countries and 500,000+ hectares of farmland, they bring proven technology to our mission. Together, we’ve launched the Magnetic Yield Alliance—our latest initiative to revolutionize farming with zero-cost, high-return solutions.          </p>
+          <p>
+At Barak AgriTech, our mission is to empower farmers with advanced tools and knowledge. We aim to increase crop yields by 15–30%, reduce water and input costs by up to 50%, and unlock farming potential in saline lands—all while promoting a greener Pakistan. From magnetic technologies to future innovations like drones and AI, we’re here to lead the way.          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
